beeaabadd3c262cf144dd72eabc0a484c5084f4d
Auxeno <alex1goddard@gmail.com>
2024-12-05 18:26:45 +0000

    add rppo

--- lotus/algos/rppo.py
+++ lotus/algos/rppo.py
@@ -1,7 +1,8 @@
 """
-PPO Agent
+RPPO Agent
 
-Proximal policy optimisation agent.
+Recurrent PPO agent.
+Does not support environment timeouts via truncation.
 
 Features:
 - Reduced output layer variance
@@ -19,14 +20,14 @@ import optax
 from distrax import Categorical
 from chex import Scalar, Array, ArrayTree, PRNGKey
 
-from ..common.agent import OnPolicyAgent
-from ..common.networks import MLP, SimpleCNN
+from ..common.agent import RecurrentOnPolicyAgent
+from ..common.networks import MLP, SimpleCNN, GRUCore
 from ..common.utils import AgentState, Transition, Logs
 
 
 ### Network ###
     
-class ActorCriticNetwork(nn.Module):
+class RecurrentActorCriticNetwork(nn.Module):
     """Combined actor critic networks."""
 
     action_dim: int
@@ -34,44 +35,56 @@ class ActorCriticNetwork(nn.Module):
     hidden_dims: Sequence[int]
 
     @nn.compact
-    def __call__(self, observations: Array):
+    def __call__(self, rnn_state: Array, observations: Array, resets: Array):
         # Use CNN for pixel observations
         if self.pixel_obs:
-            torso = SimpleCNN(activation_fn=nn.tanh)
+            torso = SimpleCNN(activation_fn=nn.relu)
         else:
             torso = lambda x: x
         x = torso(observations)
 
-        # MLP core
-        x = MLP(self.hidden_dims, activation_fn=nn.tanh)(x)
+        # Apply MLP to torso output
+        x = MLP(self.hidden_dims, activation_fn=nn.relu)(x)
+
+        # GRU core
+        new_rnn_state, x = GRUCore()(rnn_state, (x, resets))
 
         # Separate actor and critic heads
         logits = nn.Dense(self.action_dim, kernel_init=orthogonal(0.01))(x)
         value = nn.Dense(1, kernel_init=orthogonal(1.0))(x)
 
-        return logits, value.squeeze(-1)
-    
+        return new_rnn_state, logits, value.squeeze(-1)
+
 
 ### Agent State ###
 
 # Alias
-PPOState = AgentState
+RPPOState = AgentState
 
 
 ### Environment Transition ###
 
 @dataclass
-class PPOTransition(Transition):
-    """Extended transition for better efficiency."""
-    log_probs: Array = field(pytree_node=True)
-    values: Array = field(pytree_node=True)
+class RPPOTransition:
+    """Extended transition for better efficiency. Combined dones flag."""
+
+    observations: Array = field(True)
+    next_observations: Array = field(True)
+    actions: Array = field(True)
+    rewards: Array = field(True)
+    dones: Array = field(True)
+    log_probs: Array = field(True)
+    values: Array = field(True)
+    prev_dones: Array = field(True)
+    initial_rnn_state: Array = field(True, default=jnp.nan)
+    final_value: Array = field(True, default=jnp.nan)
 
 
 ### Agent ###
 
 @dataclass
-class PPO(OnPolicyAgent):
-    """PPO agent."""
+class RPPO(RecurrentOnPolicyAgent):
+    """Recurrent PPO agent."""
 
     num_epochs: int      = field(False, default=10)   # Number of training epochs per rollout
     num_minibatches: int = field(False, default=1)    # Number of minibatches per epoch
@@ -90,11 +103,13 @@ class PPO(OnPolicyAgent):
         # Create network
         action_dim = self.action_space.n
         obs_shape = self.observation_space.shape
-        sample_obs = self.observation_space.sample(jax.random.PRNGKey(0))
+        sample_obs = self.observation_space.sample(jax.random.PRNGKey(0))[None, None, ...]
+        sample_resets = jnp.zeros((1, self.num_envs), dtype=bool)
+        sample_hidden = GRUCore.initialize_carry(self.num_envs, self.hidden_dims[-1])
         if len(obs_shape) not in (1, 3):
             raise Exception(f"Invalid observation space shape: {obs_shape}.")
         pixel_obs = len(obs_shape) == 3
-        network = ActorCriticNetwork(
+        network = RecurrentActorCriticNetwork(
             action_dim, pixel_obs, self.hidden_dims
         )
 
@@ -112,11 +127,42 @@ class PPO(OnPolicyAgent):
         )
 
         # Create and return AgentState
-        return PPOState.create(
+        return RPPOState.create(
             apply_fn=network.apply,
-            params=network.init(key, sample_obs[None, ...]),
+            params=network.init(key, sample_hidden, sample_obs, sample_resets),
             tx=optimizer
         )
+    
+    def select_action(
+        self,
+        key: PRNGKey, 
+        agent_state: RPPOState,
+        rnn_state: Array,
+        observations: Array,
+        dones: Array
+    ) -> Array:
+        """Select action using recurrent policy network."""
+
+        # Forward pass through network, None adds a dummy timestep dimension
+        new_rnn_state, logits, values = agent_state.apply_fn(
+            agent_state.params, rnn_state, observations[None, ...], dones[None, ...]
+        )
+        
+        # Create categorical distribution
+        dist = Categorical(logits=logits.squeeze(0))
+        
+        # Sample action from policy distribution
+        actions = dist.sample(seed=key)
+        
+        # Calculate log probs
+        log_probs = dist.log_prob(actions)
+
+        return {
+            'actions': actions, 
+            'new_rnn_state': new_rnn_state, 
+            'log_probs': log_probs, 
+            'values': values.squeeze(0)
+        }
     
     def rollout(
         self,
@@ -129,41 +175,50 @@ class PPO(OnPolicyAgent):
             """Scannable single vectorised environment step."""
 
             # Unpack carry
-            key, env_states, observations = (
-                carry['key'], carry['env_states'], carry['observations']
+            key, env_states, observations, rnn_state, prev_dones = (
+                carry['key'], 
+                carry['env_states'], 
+                carry['observations'],
+                carry['rnn_state'],
+                carry['prev_dones']
             )
 
             # RNG
             key, key_action, key_step = jax.random.split(key, 3)
 
             # Action selection
-            action_result = self.select_action(key_action, agent_state, observations)
-            actions = action_result['actions']
+            action_result = self.select_action(
+                key_action, agent_state, rnn_state, observations, prev_dones
+            )
 
             # Environment step
-            step_result = self.env_step(key_step, env_states, actions)
+            step_result = self.env_step(key_step, env_states, action_result['actions'])
+
+            # Calculate dones
+            dones = jnp.logical_or(step_result['terminations'], step_result['truncations'])
 
             # Build carry for next step
             new_carry = {
                 'key': key,
                 'env_states': step_result['next_env_states'],
-                'observations': step_result['next_observations']
+                'observations': step_result['next_observations'],
+                'rnn_state': action_result['new_rnn_state'],
+                'prev_dones': dones
             }
 
             # Build transition
-            transition = PPOTransition(
+            transition = RPPOTransition(
                 observations=observations,
                 next_observations=step_result['next_observations'],
-                actions=actions,
+                actions=action_result['actions'],
                 rewards=step_result['rewards'],
-                terminations=step_result['terminations'],
-                truncations=step_result['truncations'],
+                dones=dones,
                 log_probs=action_result['log_probs'],
-                values=action_result['values']
+                values=action_result['values'],
+                prev_dones=prev_dones
             )
 
             # Build logs for step
-            dones = jnp.logical_or(step_result['terminations'], step_result['truncations'])
             logs = Logs(rewards=step_result['rewards'], dones=dones)
 
             return new_carry, (transition, logs)
@@ -173,6 +228,9 @@ class PPO(OnPolicyAgent):
             f=rollout_step, init=initial_carry, xs=None, length=self.rollout_steps
         )
 
+        # Add initial RNN state to batch of experiences
+        experiences = experiences.replace(initial_rnn_state=initial_carry['rnn_state'])
+
         # Return experiences, logs and final carry
         return {
             'experiences': experiences,
@@ -180,60 +238,40 @@ class PPO(OnPolicyAgent):
             'logs': logs
         }
 
-    def select_action(
-        self, 
-        key: PRNGKey, 
-        agent_state: AgentState,
-        observations: Array
-    ):
-        """Action selection logic."""
-
-        # Forward pass through network
-        logits, values = agent_state.apply_fn(agent_state.params, observations)
-        
-        # Create categorical distribution
-        dist = Categorical(logits=logits)
-        
-        # Sample action from policy distribution
-        actions = dist.sample(seed=key)
-        
-        # Calculate log probs
-        log_probs = dist.log_prob(actions)
-        
-        return {'actions': actions, 'log_probs': log_probs, 'values': values}
-    
     def calculate_gae(
         self,
-        agent_state: AgentState, 
-        batch: ArrayTree
+        batch: ArrayTree,
     ) -> Tuple:
         """Compute advantage and returns using GAE."""
 
         def gae_step(advantage, transition) -> Tuple[Array, ArrayTree]:
             """Scannable GAE step."""
             # Unpack transition
-            reward, termination, truncation, value, next_value = transition
+            reward, done, value, next_value = transition
 
             # Mask for non-terminal transitions
-            non_termination, non_truncation = 1.0 - termination, 1.0 - truncation
+            non_terminal = 1.0 - done
 
             # Compute delta (TD residual)
-            delta = reward + self.gamma * next_value * non_termination - value
+            delta = reward + self.gamma * next_value * non_terminal - value
 
             # Update advantage
-            advantage = delta + self.gamma * self.gae_lambda * non_termination * non_truncation * advantage
+            advantage = delta + self.gamma * self.gae_lambda * non_terminal * advantage
             return advantage, advantage
 
-        # Values for current and next observations
-        _, next_values = agent_state.apply_fn(agent_state.params, batch.next_observations)
-
+        # Values for next observations
+        next_values = jnp.concat((batch.values[1:], batch.final_value[None, ...]), axis=0)
+        
         # Initialise GAE scan parameters
         initial_carry = jnp.zeros(self.num_envs)
-        transitions = (batch.rewards, batch.terminations, batch.truncations, batch.values, next_values)
+        transitions = (batch.rewards, batch.dones, batch.values, next_values)
 
         # Compute advantages via reversed scan
         _, advantages = jax.lax.scan(
-            f=gae_step, init=initial_carry, xs=transitions, reverse=True
+            gae_step,
+            initial_carry,
+            transitions,
+            reverse=True
         )
 
         # Calculate returns
@@ -244,31 +282,37 @@ class PPO(OnPolicyAgent):
     def learn(
         self,
         key: PRNGKey, 
-        agent_state: AgentState, 
-        batch: ArrayTree
-    ) -> AgentState:
+        agent_state: RPPOState, 
+        batch: ArrayTree, 
+    ) -> RPPOState:
         """Update agent parameters with a batch of experience."""
         
-        def minibatch_update(agent_state: AgentState, mb_indices: Array) -> AgentState:
+        def minibatch_update(agent_state: RPPOState, mb_indices: Array) -> RPPOState:
             """Scannable minibatch gradient descent update."""
             
             def ppo_loss(params: ArrayTree) -> Scalar:
                 """Differentiable PPO loss function."""
+
+                # Full forward pass
+                _, logits, values = agent_state.apply_fn(
+                    params, batch.initial_rnn_state, batch.observations, batch.prev_dones
+                )
                 
-                # Forward pass
-                logits, values = agent_state.apply_fn(params, batch.observations[mb_indices])
-                
+                # Flatten batch, logits and values
+                actions, old_log_probs, logits, values = \
+                    jax.tree.map(lambda x: x.reshape(-1, *x.shape[2:]), (batch.actions, batch.log_probs, logits, values))
+
                 # Policy loss
-                distribution = Categorical(logits=logits)
-                log_probs = distribution.log_prob(batch.actions[mb_indices])
-                ratio = jnp.exp(log_probs - batch.log_probs[mb_indices])
+                distribution = Categorical(logits=logits[mb_indices])
+                log_probs = distribution.log_prob(actions[mb_indices])
+                ratio = jnp.exp(log_probs - old_log_probs[mb_indices])
                 loss_surrogate_unclipped = -advantages[mb_indices] * ratio
                 loss_surrogate_clipped = -advantages[mb_indices] * \
                     jnp.clip(ratio, 1 - self.clip_coef, 1 + self.clip_coef)
                 loss_policy = jnp.maximum(loss_surrogate_unclipped, loss_surrogate_clipped).mean()
 
                 # Value loss
-                loss_value = ((values - returns[mb_indices]) ** 2).mean()
+                loss_value = ((values[mb_indices] - returns[mb_indices]) ** 2).mean()
 
                 # Entropy bonus
                 loss_entropy = distribution.entropy().mean()
@@ -290,7 +334,7 @@ class PPO(OnPolicyAgent):
             return agent_state, loss
 
         # Compute advantages using GAE
-        advantages, returns = self.calculate_gae(agent_state, batch)
+        advantages, returns = self.calculate_gae(batch)
 
         # Normalise advantages if enabled            
         advantages = jnp.where(
@@ -299,8 +343,7 @@ class PPO(OnPolicyAgent):
             advantages
         )
 
-        # Flatten data
-        batch = jax.tree.map(lambda x: x.reshape(-1, *x.shape[2:]), batch)
+        # Flatten advantages and returns
         advantages, returns = jax.tree.map(lambda x: x.flatten(), (advantages, returns))
 
         # Create shuffled minibatch indices
@@ -313,13 +356,13 @@ class PPO(OnPolicyAgent):
         agent_state, losses = jax.lax.scan(
             f=minibatch_update, init=agent_state, xs=indices
         )
-        
+
         # Return updated agent state
         return agent_state
 
     @staticmethod
     def train(
-        agent: 'PPO',
+        agent: 'RPPO',
         seed: int = 0
     ) -> Dict:
         """Main training loop."""
@@ -346,6 +389,19 @@ class PPO(OnPolicyAgent):
                 rollout_result['carry'],
                 rollout_result['logs']
             )
+
+            # Unpack items rollout carry
+            new_rnn_state, next_observations, next_dones = (
+                new_rollout_carry['rnn_state'],
+                new_rollout_carry['observations'],
+                new_rollout_carry['prev_dones']
+            )
+
+            # Add final value to batch
+            _, _, final_value = agent_state.apply_fn(
+                agent_state.params, new_rnn_state, next_observations[None, ...], next_dones[None, ...]
+            )
+            experiences = experiences.replace(final_value=final_value.squeeze(0))
 
             # Perform learn step
             agent_state = agent.learn(key_learn, agent_state, experiences)
